b0a12b5e8007775639d012f5e98f56aa3e98e953
Mateusz Rachuta <mrachuta@thinkbox.pl>
2020-09-26 12:54:14 +0200

    Reorganized code; supressed outputs; added cleanup function

--- ansible-tools/profile-personalisator.sh
+++ ansible-tools/profile-personalisator.sh
@@ -1,33 +1,46 @@
 #!/usr/bin/env bash
 
 PACKAGE_URL=https://github.com/mrachuta/dev-null/raw/master/profile-files.tar
+
+PLUGIN_LIST=(
+  "https://github.com/luochen1990/rainbow"
+  "https://github.com/martinda/jenkinsfile-vim-syntax"
+  "https://github.com/ekalinin/dockerfile.vim"
+  "https://github.com/python-mode/python-mode"
+  "https://github.com/scrooloose/nerdcommenter"
+  "https://github.com/itchyny/lightline.vim"
+  "https://github.com/pangloss/vim-javascript"
+  "https://github.com/chr4/nginx.vim"
+)
+
 arch_name=$(echo $PACKAGE_URL | rev | cut -d'/' -f 1 | rev)
 folder_name=$(echo "$arch_name" | rev | cut -d'.' -f 2 | rev)
 curr_datetime=$(date +%Y-%m-%d-%H_%M_%S)
 
 get_package() {
 
-  echo "Downloading package with pre-configured files..."
+  echo "Downloading necessary files..."
+  echo "--> Downloading package with pre-configured files..."
 
-  wget -O "/tmp/$arch_name" $PACKAGE_URL
+  wget -qO "/tmp/$arch_name" $PACKAGE_URL
   mkdir "/tmp/$folder_name"
-  tar -xvf "/tmp/$arch_name" -C "/tmp/$folder_name"
+  tar -xf "/tmp/$arch_name" -C "/tmp/$folder_name"
 
-  echo "Package downloaded!"
+  echo -e "Files downloaded!\n"
 }
 
 configure_bashrc() {
 
-  echo "bashrc configuration..."
+  echo "Configuring bashrc..."
   echo "Backup of .bashrc available in /home/$1/bashrc-$curr_datetime"
 
   cp "/home/$1/.bashrc" "/home/$1/bashrc-$curr_datetime.bak"
-  echo "Chosing random color for machine-name background..."
+  echo "--> Chosing random color for machine-name background..."
   # Last color in table is 256, but after 229 there are
   # gray/black/white colors 
   # https://misc.flogisoft.com/bash/tip_colors_and_formatting
   color_code=$(( RANDOM % 229 ))
-  echo "$color_code"
+  echo "Selected color: $color_code"
   searched_val="D_MACHINE_COLOR=\"\\\e\[30;48;5;[[:digit:]]{1,3}m\""
   new_val="D_MACHINE_COLOR=\"\\\e\[30;48;5;${color_code}m\""
   sed -i -r "s~^$searched_val~$new_val~" "/tmp/$folder_name/.bashrc"
@@ -37,36 +50,26 @@ configure_bashrc() {
   chmod 644 "/home/$1/.bashrc"
   chown "$1":"$1" "/home/$1/.bashrc"
 
-  echo "bashrc configuration finished!"
+  echo -e "bashrc configuration finished!\n"
 
 }
 
 configure_bash_aliases() {
   
-  echo "bash_aliases configuration..."
+  echo "Configuring bash_aliases..."
+  echo "--> writing custom aliases to file..."
 
   cp "/tmp/$folder_name/.bash_aliases" "/home/$1/.bash_aliases"
   chmod 644 "/home/$1/.bash_aliases"
   chown "$1":"$1" "/home/$1/.bash_aliases"
 
-  echo "bash_aliases configuration finished"
+  echo -e "bash_aliases configuration finished!\n"
 }
 
 configure_vim() {
 
   # Good source
   # https://vimawesome.com/
-
-  plugin_list=(
-    "https://github.com/luochen1990/rainbow"
-    "https://github.com/martinda/jenkinsfile-vim-syntax"
-    "https://github.com/ekalinin/dockerfile.vim"
-    "https://github.com/python-mode/python-mode"
-    "https://github.com/scrooloose/nerdcommenter"
-    "https://github.com/itchyny/lightline.vim"
-    "https://github.com/pangloss/vim-javascript"
-    "https://github.com/chr4/nginx.vim"
-  )
 
   echo "Configuring vim..."
 
@@ -79,33 +82,47 @@ configure_vim() {
   curl -LSso "/home/$1/.vim/autoload/pathogen.vim" "https://tpo.pe/pathogen.vim"
 
   # Install molokai theme
-  wget -P "/home/$1/.vim/colors" \
+  wget -qP "/home/$1/.vim/colors" \
     "https://github.com/tomasr/molokai/raw/master/colors/molokai.vim"
 
   # Install molokayo theme
-  wget -P "/home/$1/.vim/colors" \
+  wget -qP "/home/$1/.vim/colors" \
     "https://github.com/fmoralesc/molokayo/raw/master/colors/molokayo.vim"
 
   # Install plugins
   # Do not change!
-  plugin_list_len=${#plugin_list[@]}
+  plugin_list_len=${#PLUGIN_LIST[@]}
 
   for i in $(seq 0 $((plugin_list_len - 1))); do
-    plugin_name=$(echo "${plugin_list[$i]}" | rev | cut -d'/' -f 1 | rev)
-    echo "Installing plugin: $plugin_name"
-    cd "/home/$1/.vim/bundle" && git clone "${plugin_list[$i]}"
+    plugin_name=$(echo "${PLUGIN_LIST[$i]}" | rev | cut -d'/' -f 1 | rev)
+    echo "--> Installing plugin: $plugin_name"
+    # Git sending output to stderr
+    # https://stackoverflow.com/a/37669280/9342492
+    # Submodules required for python-mode
+    cd "/home/$1/.vim/bundle" && git clone --recurse-submodules "${PLUGIN_LIST[$i]}"
   done
 
   chown -R "$1":"$1" "/home/$1/.vim"
 
-  echo "Vim configuration finished!"
+  echo -e "vim configuration finished!\n"
+}
+
+do_cleanup() {
+
+  echo "Cleanup..."
+
+  rm -Rf /tmp/$arch_name
+  rm -Rf /tmp/$folder_name
+
+  echo -e "Cleanup finished!\n"
+
 }
 
 usage() {
 
   echo -e "Script for personalisation bash & other tools\n"
   echo "Usage: profile-personalisator.sh user"
-  echo "Replace 'user' with your username"
+  echo -e "Replace 'user' with your username\n"
 
 }
 
@@ -114,11 +131,13 @@ main() {
   usage
 
   if [[ -n "$1" ]]; then
+    echo "Script started"
     get_package
     configure_bashrc "$1"
     configure_bash_aliases "$1"
     configure_vim "$1"
-    echo "Run 'source ~/.bashrc' to see the changes"
+    do_cleanup
+    echo "Run 'source ~/.bashrc' to see the changes!"
   else
     usage
   fi
